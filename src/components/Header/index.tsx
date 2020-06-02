03e56a748460c62e954c800e31e6ac3b29b78322
Amanda Polotow <amandapolotow@gmail.com>
2020-06-02 15:03:27 -0300

    Deleted unused variable

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { Container } from './styles';
 
@@ -15,9 +15,6 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
         <NavLink
           to="/"
           exact
